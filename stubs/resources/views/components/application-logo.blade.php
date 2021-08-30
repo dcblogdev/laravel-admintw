9e913652ab866d6b71edfb76e0e4c0496893243f
David Carr <dave@dcblog.dev>
2021-08-30 20:37:22 +0100

    changed default logo

--- stubs/resources/views/components/application-logo.blade.php
+++ stubs/resources/views/components/application-logo.blade.php
@@ -1,3 +1,3 @@
 <div class="text-5xl tracking-tight leading-10 font-bold text-gray-900 dark:text-white">
-  Laravel Microsoft Graph <span class="text-blue-600 dark:text-blue-400">Demo</span>
+  Admin<span class="text-blue-600 dark:text-blue-400">TW</span>
 </div>
