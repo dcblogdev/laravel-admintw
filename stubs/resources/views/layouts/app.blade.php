c768776886019ef2663ceedbb0a0b1af358eafe4
David Carr <dave@dcblog.dev>
2021-12-19 07:53:08 +0000

    use mix for assets

--- stubs/resources/views/layouts/app.blade.php
+++ stubs/resources/views/layouts/app.blade.php
@@ -5,8 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ $title ?? '' }} {{ config('app.name', 'Laravel') }}</title>
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+    <script src="{{ mix('js/app.js') }}" defer></script>
     <livewire:styles/>
 </head>
 <body>
