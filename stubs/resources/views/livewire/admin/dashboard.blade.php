1c28f545d8f5d9b1316d203f61cc4acddf5ddb39
David Carr <dave@dcblog.dev>
2022-06-07 02:31:21 +0100

    updated dashboard

--- stubs/resources/views/livewire/admin/dashboard.blade.php
+++ stubs/resources/views/livewire/admin/dashboard.blade.php
@@ -1,3 +1,11 @@
 @section('title', 'Dashboard')
 
 <h1>Dashboard</h1>
+
+<div class="card">
+    <p><b>Laravel AdminTW</b> is a free theme</p>
+    <p>AdminTW is minimal theme presenting a clean sidebar and top bar and open areas for content to be displayed.</p>
+    <p>AdminTW supports both light and dark mode based on the users OS.</p>
+    <p>Provided are blade and Laravel Livewire components for common layout / UI elements and a complete test suite (Pest PHP).</p>
+    <p>Read the <a href="https://laraveladmintw.com/docs">Documentation</a></p>
+</div>
