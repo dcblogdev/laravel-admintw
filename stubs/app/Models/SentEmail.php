d7cd43faab325bbf097c1c00e9859af107d4a28b
dave <dave@dcblog.dev>
2022-07-24 00:03:34 +0100

    added uuid trait for sent emails

--- stubs/app/Models/SentEmail.php
+++ stubs/app/Models/SentEmail.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Models\Traits\HasUuid;
 use Database\Factories\SentEmailFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
@@ -9,6 +10,7 @@ use Illuminate\Database\Eloquent\Model;
 class SentEmail extends Model
 {
     use HasFactory;
+    use HasUuid;
 
     protected $guarded = [];
 
