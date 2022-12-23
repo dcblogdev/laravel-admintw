6263b42e0b92bd0f6cc4d6f1a11a2821eba03444
David Carr <dave@dcblog.dev>
2022-12-23 01:08:26 +0000

    remove mix and use vite in layouts

--- stubs/resources/views/layouts/plain.blade.php
+++ stubs/resources/views/layouts/plain.blade.php
@@ -4,9 +4,8 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
-        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+        @vite(['resources/sass/app.scss', 'resources/js/app.js'])
         @stack('styles')
-        <script src="{{ mix('js/app.js') }}" defer></script>
         @stack('scripts')
     </head>
     <body>
