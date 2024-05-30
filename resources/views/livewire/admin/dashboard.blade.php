7e63a6616a64ce9c9139ae2955595c3aa9444eeb
David Carr <dave@dcblog.dev>
2024-05-30 22:38:53 +0100

    reset dashboard.blade.php

--- resources/views/livewire/admin/dashboard.blade.php
+++ resources/views/livewire/admin/dashboard.blade.php
@@ -5,34 +5,4 @@
         {{ __("You're logged in!") }}
     </div>
 
-    <x-dialog>
-        <x-dialog.open>
-            <x-button>Open</x-button>
-        </x-dialog.open>
-
-        <x-dialog.panel>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab aspernatur commodi cumque cupiditate dolore est explicabo illum in maxime molestiae molestias pariatur praesentium, quas saepe totam unde veniam vitae voluptatibus?</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab aspernatur commodi cumque cupiditate dolore est explicabo illum in maxime molestiae molestias pariatur praesentium, quas saepe totam unde veniam vitae voluptatibus?</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab aspernatur commodi cumque cupiditate dolore est explicabo illum in maxime molestiae molestias pariatur praesentium, quas saepe totam unde veniam vitae voluptatibus?</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab aspernatur commodi cumque cupiditate dolore est explicabo illum in maxime molestiae molestias pariatur praesentium, quas saepe totam unde veniam vitae voluptatibus?</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab aspernatur commodi cumque cupiditate dolore est explicabo illum in maxime molestiae molestias pariatur praesentium, quas saepe totam unde veniam vitae voluptatibus?</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab aspernatur commodi cumque cupiditate dolore est explicabo illum in maxime molestiae molestias pariatur praesentium, quas saepe totam unde veniam vitae voluptatibus?</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab aspernatur commodi cumque cupiditate dolore est explicabo illum in maxime molestiae molestias pariatur praesentium, quas saepe totam unde veniam vitae voluptatibus?</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab aspernatur commodi cumque cupiditate dolore est explicabo illum in maxime molestiae molestias pariatur praesentium, quas saepe totam unde veniam vitae voluptatibus?</p>
-        </x-dialog.panel>
-
-        <x-dialog.footer>
-            <x-dialog.close>
-                <x-button>Close</x-button>
-            </x-dialog.close>
-        </x-dialog.footer>
-    </x-dialog>
-
-    <select>
-        <option value="">Select</option>
-        @foreach(App\Enums\Country::cases() as $country)
-            <option value="{{ $country->value }}">{{ $country->label() }}</option>
-        @endforeach
-    </select>
-
 </div>
