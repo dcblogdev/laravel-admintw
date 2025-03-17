0f9a6c9c5bd27c1e21d4f89a90a3a3130a12df67
David Carr <dave@dcblog.dev>
2025-03-17 01:39:13 +0000

    refactor button components and update navigation icons for consistency

--- resources/views/components/layouts/app/navigation.blade.php
+++ resources/views/components/layouts/app/navigation.blade.php
@@ -1,38 +1,30 @@
-<button @click.stop="sidebarOpen = !sidebarOpen" class="md:hidden focus:outline-none pl-1 pt-4 pr-2">
-    <svg class="h-6 w-6 transition ease-in-out duration-150 text-white" xmlns="http://www.w3.org/2000/svg"
-         fill="none" viewBox="0 0 24 24" stroke="currentColor">
-        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"/>
-    </svg>
-</button>
-
-<div class="w-full rounded-sm px-4 py-1 text-center">
-    <h3 class="mb-1 font-semibold text-white">
-        <a href="{{ route('dashboard') }}" class="text-white font-bold">
-            {{ config('app.name') }}
-        </a>
-    </h3>
-</div>
 
 @can('view_dashboard')
-    <x-nav.link route="dashboard" icon="fas fa-home">{{ __('Dashboard') }}</x-nav.link>
+    <x-nav.link route="dashboard" icon="home">{{ __('Dashboard') }}</x-nav.link>
 @endcan
 
-@if(can('view_system_settings') || can('view_roles') || can('view_audit_trails') || can('view_sent_emails'))
-    <x-nav.group label="{{__('Settings')}}" route="admin.settings" icon="fas fa-cogs">
-        @can('view_audit_trails')
-            <x-nav.group-item route="admin.settings.audit-trails.index" icon="far fa-circle">{{__('Audit Trails')}}</x-nav.group-item>
-        @endcan
-
-        @can('view_roles')
-            <x-nav.group-item route="admin.settings.roles.index" icon="far fa-circle">{{__('Roles')}}</x-nav.group-item>
-        @endcan
-
-        @can('view_system_settings')
-            <x-nav.group-item route="admin.settings" icon="far fa-circle">{{__('System Settings')}}</x-nav.group-item>
-        @endcan
-    </x-nav.group>
+@if(can('view_system_settings') || can('view_roles') || can('view_audit_trails'))
+    <div class="px-3 pt-5 pb-2 text-xs font-semibold tracking-wider text-gray-500 uppercase">
+        {{ __('Settings') }}
+    </div>
 @endif
 
+@can('view_audit_trails')
+    <x-nav.link route="admin.settings.audit-trails.index" icon="identification">{{ __('Audit Trails') }}</x-nav.link>
+@endcan
+
+@can('view_roles')
+    <x-nav.link route="admin.settings.roles.index" icon="archive-box">{{ __('Roles') }}</x-nav.link>
+@endcan
+
+@can('view_system_settings')
+    <x-nav.link route="admin.settings" icon="cog">{{ __('System Settings') }}</x-nav.link>
+@endcan
+
+<div class="px-3 pt-5 pb-2 text-xs font-semibold tracking-wider text-gray-500 uppercase">
+    {{ __('Account') }}
+</div>
+
 @can('view_users')
-    <x-nav.link route="admin.users.index" icon="fas fa-users">{{__('Users')}}</x-nav.link>
+    <x-nav.link route="admin.users.index" icon="users">{{ __('Users') }}</x-nav.link>
 @endcan
--- resources/views/components/layouts/guest.blade.php
+++ resources/views/components/layouts/guest.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title') - {{ config('app.name', 'Laravel') }}</title>
-    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="dark font-sans text-gray-900 antialiased">
         <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
