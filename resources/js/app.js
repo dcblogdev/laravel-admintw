d2fd7d264b2b3db76d9237b2b67f7712406cb175
David Carr <dave@dcblog.dev>
2025-03-17 21:00:38 +0000

    add Alpine.js UI plugin for enhanced component functionality

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,10 @@
 import './bootstrap';
 import './dark';
 import 'flatpickr';
+import ui from '@alpinejs/ui';
 
 import * as FilePond from 'filepond';
 window.FilePond = FilePond;
+
+Alpine.plugin(ui);
 
