c21c74993e08214f7152d73276d133ba084d3fbc
David Carr <dave@dcblog.dev>
2025-03-19 13:53:14 +0000

    remove unused 2-column layout from dashboard

--- resources/views/livewire/admin/dashboard.blade.php
+++ resources/views/livewire/admin/dashboard.blade.php
@@ -5,14 +5,4 @@
         {{ __("You're logged in!") }}
     </div>
 
-
-    <x-2col>
-        <x-slot name="left">
-            <p class="bg-primary text-white p-1">Left</p>
-        </x-slot>
-        <x-slot name="right">
-            <p class="bg-primary text-white p-1">Right</p>
-        </x-slot>
-    </x-2col>
-
 </div>
